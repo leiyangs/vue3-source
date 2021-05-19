b1437770f8f9238fce7e00280d4ee12537e5507d
yanglei <yanglei@shjinjia.com>
2021-05-19 17:56:06 +0800

    store home

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
-    home
+    <homeHeader/>
+    <swiper/>
+    <list/>
     <van-button type="primary" url="/vant/mobile.html">URL 跳转</van-button>
   </div>
 </template>
 
-<script>
-import { defineComponent } from "vue";
-import data from '@/static/data.json';
 
-export default defineComponent({ 
+<script lang="ts">
+import { defineComponent } from 'vue'
+import data from '@/static/data.json';
+import homeHeader from './header.vue';
+import list from './list.vue';
+import swiper from './swiper.vue';
+
+// defineCompoent包裹可以有提示(此提示是ts提示，不是vuter提示)
+export default defineComponent({
+  components: {
+    homeHeader,
+    list,
+    swiper
+  },
   setup() {
     console.log(data)
   },
 })
 </script>
+
 
