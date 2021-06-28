a65ade113d52cb52ff0fda7510d04f383d95f4da
leiyangs <15135210985@163.com>
2021-06-28 14:57:52 +0800

    mountElement

--- src/mockVue/runtime-core/createRenderer.ts
+++ src/mockVue/runtime-core/createRenderer.ts
@@ -10,11 +10,11 @@ import { createComponentInstance, setupComponent } from "./component";
  * @param options 接收不同平台传递的参数，实现跨平台操作
  * @returns
  */
-export function createRenderer(options: object) {
+export function createRenderer(options: any) {
   return baseCreateRenderer(options);
 }
 
-function baseCreateRenderer(options: object) {
+function baseCreateRenderer(options: any) {
   const render = (vNode: any, container: any) => {
     patch(null, vNode, container);
   };
@@ -30,15 +30,59 @@ function baseCreateRenderer(options: object) {
     effect(function() {
       // 判断组件是第一次加载，还是已经加载过更新组件
       if (!instance.isMounted) {
+        // 创建
         const subTree = (instance.subTree = instance.render());
         patch(null, subTree, container);
         instance.isMounted = true;
       } else {
+        // 更新 diff操作，两棵树的比对
+        let prev = instance.subTree;
+        let next = instance.render();
       }
     });
   }
 
-  const mountElement = (n1: any, container: any) => {};
+  /**
+   * 挂载多个子节点
+   * @param container
+   * @param children
+   */
+  function mountChildren(children: any, container: any) {
+    for (let i = 0; i < children.length; i++) {
+      const element = children[i];
+      patch(null, element, container);
+    }
+  }
+  // 解构已有方法
+  const {
+    createElement: hostCreateElement, // 别名
+    patchProp: hostPatchProp,
+    setElementText: hostSetElementText,
+    insert: hostInsert,
+    remove: hostRemove,
+  } = options;
+  const mountElement = (vnode: any, container: any) => {
+    let { shapeFlag, props } = vnode;
+    let el = (vnode.el = hostCreateElement(vnode.type));
+    // 创建子节点
+    if (shapeFlag & shapeFlags.TEXT_CHILDREN) {
+      // 一个儿子
+      hostSetElementText(el, vnode.children);
+    } else if (shapeFlag & shapeFlags.ARRAY_CHILDREN) {
+      // 多个儿子
+      mountChildren(vnode.children, el);
+    }
+    // 如果有属性
+    if (props) {
+      for (const key in props) {
+        if (Object.prototype.hasOwnProperty.call(props, key)) {
+          const element = props[key];
+          hostPatchProp(el, key, null, element);
+        }
+      }
+    }
+    hostInsert(el, container);
+  };
   const patchElement = (n1: any, n2: any, container: any) => {};
   const mountComponent = (initialVnode: any, container: any) => {
     // 组件挂载逻辑
